3a1cadfd71823d34b69b1db6aba1625a99d54dc7
Siddharth Mittal <90185630+caligo-phantom@users.noreply.github.com>
2022-02-02 14:31:13 +0530

    Add files via upload

---
+++ scrabble.c
@@ -0,0 +1,62 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // assigns points to each character respectively
+    char alpha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    for (int i = 0; i < 26; i++)
+    {
+        POINTS[i] = (int) alpha[i];
+    }
+
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // Prints the either the winner or determines whether it is a tie
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // Compute and return score for string
+    int score = 0;
+
+    // calculates the score and rejects alphanumeric letters
+    for (int j = 0, n = strlen(word); j < n; j++)
+    {
+        if (isalpha(word[j]))
+        {
+            score += toupper(word[j]);
+        }
+        else if (isalnum(word[j]))
+        {
+            score += 0;
+        }
+    }
+
+    return score;
+}
